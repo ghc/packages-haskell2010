bf349ebf600ce26970a228b7c08391e9a9fe4ff6
Simon Marlow <marlowsd@gmail.com>
2010-07-01 07:55:37 +0000

    omit the GHC-specific notes from the docs for mallocForeignPtr

--- Foreign/ForeignPtr.hs
+++ Foreign/ForeignPtr.hs
@@ -23,4 +23,23 @@ module Foreign.ForeignPtr (
         , mallocForeignPtrArray
         , mallocForeignPtrArray0
   ) where
-import "base" Foreign.ForeignPtr
+
+import qualified "base" Foreign.ForeignPtr as Base
+import "base" Foreign.ForeignPtr hiding (mallocForeignPtr)
+import "base" Foreign (Storable)
+
+-- SDM: local copy of the docs for mallocForeignPtr, to omit the
+-- GHC-specific bits.
+
+mallocForeignPtr :: Storable a => IO (ForeignPtr a)
+-- ^ Allocate some memory and return a 'ForeignPtr' to it.  The memory
+-- will be released automatically when the 'ForeignPtr' is discarded.
+--
+-- 'mallocForeignPtr' is equivalent to
+--
+-- >    do { p <- malloc; newForeignPtr finalizerFree p }
+-- 
+-- although it may be implemented differently internally: you may not
+-- assume that the memory returned by 'mallocForeignPtr' has been
+-- allocated with 'Foreign.Marshal.Alloc.malloc'.
+mallocForeignPtr = Base.mallocForeignPtr
