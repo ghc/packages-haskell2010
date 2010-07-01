d0402104379c87026f1ef5e37ebfff718b9a3471
Simon Marlow <marlowsd@gmail.com>
2010-07-01 12:53:05 +0000

    touchForeignPtr: remove GHC-specific information

--- Foreign/ForeignPtr.hs
+++ Foreign/ForeignPtr.hs
@@ -25,7 +25,7 @@ module Foreign.ForeignPtr (
   ) where
 
 import qualified "base" Foreign.ForeignPtr as Base
-import "base" Foreign.ForeignPtr hiding (mallocForeignPtr)
+import "base" Foreign.ForeignPtr hiding (mallocForeignPtr, touchForeignPtr)
 import "base" Foreign (Storable)
 
 -- SDM: local copy of the docs for mallocForeignPtr, to omit the
@@ -43,3 +43,28 @@ mallocForeignPtr :: Storable a => IO (ForeignPtr a)
 -- assume that the memory returned by 'mallocForeignPtr' has been
 -- allocated with 'Foreign.Marshal.Alloc.malloc'.
 mallocForeignPtr = Base.mallocForeignPtr
+
+-- SDM: reproduced documentation for touchForeignPtr without reference
+-- to Haskell finalizers and MVars.
+
+touchForeignPtr :: ForeignPtr a -> IO ()
+-- ^This function ensures that the foreign object in
+-- question is alive at the given place in the sequence of IO
+-- actions. In particular 'Foreign.ForeignPtr.withForeignPtr'
+-- does a 'touchForeignPtr' after it
+-- executes the user action.
+-- 
+-- Note that this function should not be used to express dependencies
+-- between finalizers on 'ForeignPtr's.  For example, if the finalizer
+-- for a 'ForeignPtr' @F1@ calls 'touchForeignPtr' on a second
+-- 'ForeignPtr' @F2@, then the only guarantee is that the finalizer
+-- for @F2@ is never started before the finalizer for @F1@.  They
+-- might be started together if for example both @F1@ and @F2@ are
+-- otherwise unreachable.
+--
+-- In general, it is not recommended to use finalizers on separate
+-- objects with ordering constraints between them.  To express the
+-- ordering robustly requires explicit synchronisation between
+-- finalizers.
+--
+touchForeignPtr = Base.touchForeignPtr
